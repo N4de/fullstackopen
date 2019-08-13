3d27a0b42752be3b929ffa927971fb035b561b35
N4de <jaAnnala@gmail.com>
2019-08-13 20:32:36 +0300

    unicafe done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,11 +11,24 @@ const Button = ({text, onClick}) => {
     );
 }
 
+const Statistic = (props) => (
+    <tr>
+        <td>{props.name}</td> 
+        <td>{props.value}</td>
+    </tr>
+);
+    
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad;
+    const average = (good - bad) / all;
+    const positive = (good / all) * 100 + '%';
+
 
   return (
     <div>
@@ -24,10 +37,38 @@ const App = () => {
         <Button text="neutral" onClick={() => setNeutral(neutral + 1)}></Button>
         <Button text="bad" onClick={() => setBad(bad + 1)}></Button>
       <Title title={'statistics'}/>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
+      {all === 0 ? 
+        <p>No feedback given</p>
+        :
+        <table>
+            <tbody>
+                <Statistic
+                name={'good'}
+                value={good}
+                />
+                <Statistic
+                name={'neutral'}
+                value={neutral}
+                />
+                <Statistic 
+                name={'bad'}
+                value={bad}
+                />
+                <Statistic 
+                name={'all'}
+                value={all}
+                />
+                <Statistic 
+                name={'average'}
+                value={average}
+                />
+                <Statistic 
+                name={'positive'}
+                value={positive}
+                />
+            </tbody>
+        </table>
+        }
     </div>
   )
 }

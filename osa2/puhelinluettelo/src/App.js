c286fccf571b7e421951bcd999964ee8acd8ce74
N4de <jaAnnala@gmail.com>
2019-08-31 17:09:53 +0300

    2.16: puhelinluettelo step8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
+import personService from './services/persons';
 
 import People from './people';
 import Filter from './filter';
@@ -12,11 +13,11 @@ const App = () => {
   const [ filter, setNewFilter ] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService
+      .getAll()
+        .then( initialPersons => {
+          setPersons(initialPersons)
+        })
   }, [])
 
   const peopleToShow = filter 
@@ -37,7 +38,8 @@ const App = () => {
         number: newNumber
       }
      
-      axios.post('http://localhost:3001/persons', newContact)
+      personService
+        .create(newContact)
         .then(
           setPersons(persons.concat(newContact))
         );
